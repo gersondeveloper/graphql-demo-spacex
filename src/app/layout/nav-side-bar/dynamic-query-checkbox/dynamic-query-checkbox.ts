c1f6d4855f85402512b976c27d5a56c349b0e3de
FooOperator <foxoperator1@gmail.com>
2025-08-04 17:31:10 +0100

    makes checkboxes more presentable (#3)

--- src/app/layout/nav-side-bar/dynamic-query-checkbox/dynamic-query-checkbox.ts
+++ src/app/layout/nav-side-bar/dynamic-query-checkbox/dynamic-query-checkbox.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-query-checkbox',
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-query-checkbox.scss'
 })
 export class DynamicQueryCheckbox {
+  isChecked: boolean = true;
+  queryField = input<QueryBuilderField>();
+  computedQueryFieldName = computed<string>(() => this.queryField()?.displayName ?? "Not found");
+
   onCheckboxChanged(event: any) {
-    
+    console.log(`${this.computedQueryFieldName} is ${event.target.checked}`);
+    this.isChecked = !this.isChecked;
+    // buildSchema()
+    // when it's checked - add to the query
+    // when it's unchecked - remove from the query
+    // add a way to prop drill some sort of callback that acknowledges the state change and warns the query builder about it
   }
 }
