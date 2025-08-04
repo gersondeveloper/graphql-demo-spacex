b46262ec2dd970686464931db9c5853a86c31d9b
FooOperator <foxoperator1@gmail.com>
2025-08-04 17:29:49 +0100

    generates checkboxes based on incoming schema from parent component (#3)

--- src/app/layout/nav-side-bar/nav-side-bar.ts
+++ src/app/layout/nav-side-bar/nav-side-bar.ts
@@ -1,6 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { DynamicQueryCheckbox } from "app/layout/nav-side-bar/dynamic-query-checkbox/dynamic-query-checkbox";
-import { ThemeToggle } from "app/layout/theme-toggle/theme-toggle";
+import {Component, inject, input, ViewEncapsulation} from '@angular/core';
+import {DynamicQueryCheckbox} from "app/layout/nav-side-bar/dynamic-query-checkbox/dynamic-query-checkbox";
+import {ThemeToggle} from "app/layout/theme-toggle/theme-toggle";
+import {GridPageService} from '@services/grid-page.service';
+import {ColDef} from 'ag-grid-community';
 
 interface NavLink {
   goTo: string;
@@ -18,10 +20,23 @@ interface NavLink {
   encapsulation: ViewEncapsulation.None,
 })
 export class NavSideBar {
+  protected readonly Object = Object;
+  gridPageService = inject(GridPageService);
+  schema = input<ColDef>();
+
+  onFetchSubmission(event: any) {
+    event.preventDefault();
+    console.log('will submit ' + this.schema);
+  }
+
   navLinks: NavLink[] = [
-    { goTo: "", pageName: "Home" },
-    { goTo: "/rockets", pageName: "Rockets" },
-    { goTo: "/launches", pageName: "Launches" },
-    { goTo: "/about", pageName: "About" },
+    {goTo: "", pageName: "Home"},
+    {goTo: "/rockets", pageName: "Rockets"},
+    {goTo: "/launches", pageName: "Launches"},
+    {goTo: "/about", pageName: "About"},
   ]
+
+  shouldRenderQueryBuilder() {
+    return this.schema() !== undefined;
+  }
 }
