0d2a3ee5d845904a138712beb0c4b7c73d14a010
FooOperator <foxoperator1@gmail.com>
2025-08-06 08:54:44 +0100

    root component now provides service that will allow the query building (#3)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import {Component, computed, input, OnInit, Signal, signal} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {RouterOutlet, ActivatedRoute, Router, NavigationEnd} from '@angular/router';
 import {NavSideBar} from "app/layout/nav-side-bar/nav-side-bar";
 import {filter, map} from 'rxjs';
 import {ColDef} from 'ag-grid-community';
+import {GridPageService} from '@services/grid-page.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,15 @@ import {ColDef} from 'ag-grid-community';
     RouterOutlet,
     NavSideBar,
   ],
+  providers: [
+    GridPageService
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
   protected readonly title = signal('graphql-demo-spacex');
-  schema: ColDef | undefined;
+  schema: ColDef[] | undefined;
 
   constructor(private route: ActivatedRoute, private router: Router) {
   }
