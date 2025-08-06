7908cc02c51bc611fac68e1ca26e0c6b1b9364eb
FooOperator <foxoperator1@gmail.com>
2025-08-06 16:11:42 +0100

    expands data passed through routes so it's easier to implement the new shared grid page service (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,14 +11,22 @@ export const routes: Routes = [
   {
     path: 'launches',
     loadComponent: () => import('./features/launches-page/launches-page').then(m => m.LaunchesPage),
-    data: { schema: LAUNCHES_COLUMN_DEFS },
+    data: {
+      queryName: "GetPastLaunches",
+      queryMethod: "pastLaunches",
+      schema: LAUNCHES_COLUMN_DEFS
+    },
     title: 'Launches',
 
   },
   {
     path: 'rockets',
     loadComponent: () => import('./features/rockets/rockets-page').then(m => m.RocketsPageComponent),
-    data: { schema: ROCKETS_COLUMN_DEFS },
+    data: {
+      queryName: "GetRockets",
+      queryMethod: "rockets",
+      schema: ROCKETS_COLUMN_DEFS
+    },
     title: 'Rockets'
   },
   {
