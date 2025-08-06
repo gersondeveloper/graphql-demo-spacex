cb44b09cef741dd898602cd15978656a2edf7fa2
FooOperator <foxoperator1@gmail.com>
2025-08-06 08:56:45 +0100

    minor css changes and moved query fields to a new component (query-builder)(#3)

--- src/app/layout/nav-side-bar/nav-side-bar.ts
+++ src/app/layout/nav-side-bar/nav-side-bar.ts
@@ -1,8 +1,9 @@
-import {Component, inject, input, ViewEncapsulation} from '@angular/core';
+import {Component, inject, Input, input, model, OnInit, ViewEncapsulation} from '@angular/core';
 import {DynamicQueryCheckbox} from "app/layout/nav-side-bar/dynamic-query-checkbox/dynamic-query-checkbox";
 import {ThemeToggle} from "app/layout/theme-toggle/theme-toggle";
 import {GridPageService} from '@services/grid-page.service';
 import {ColDef} from 'ag-grid-community';
+import {QueryBuilder} from './query-builder/query-builder';
 
 interface NavLink {
   goTo: string;
@@ -13,20 +14,17 @@ interface NavLink {
   selector: 'app-nav-side-bar',
   imports: [
     ThemeToggle,
-    DynamicQueryCheckbox
+    QueryBuilder
   ],
   templateUrl: './nav-side-bar.html',
   styleUrl: './nav-side-bar.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class NavSideBar {
-  protected readonly Object = Object;
-  gridPageService = inject(GridPageService);
-  schema = input<ColDef>();
+export class NavSideBar implements OnInit {
+  schema = input<ColDef[]>();
 
-  onFetchSubmission(event: any) {
-    event.preventDefault();
-    console.log('will submit ' + this.schema);
+  ngOnInit(): void {
+
   }
 
   navLinks: NavLink[] = [
@@ -34,7 +32,7 @@ export class NavSideBar {
     {goTo: "/rockets", pageName: "Rockets"},
     {goTo: "/launches", pageName: "Launches"},
     {goTo: "/about", pageName: "About"},
-  ]
+  ];
 
   shouldRenderQueryBuilder() {
     return this.schema() !== undefined;
